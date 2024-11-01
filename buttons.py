8039c2972174894cc17ccb9bec7849b443da72ef
Mariya-Mukhitdinova <mukhitdinovamariya@gmail.com>
2024-11-01 12:41:46 +0500

    first

--- buttons.py
+++ buttons.py
@@ -2,9 +2,11 @@ from telebot import types
 
 def main_menu():
     kb = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    button1 = types.KeyboardButton(text="UZS_USD")
-    button2 = types.KeyboardButton(text="USD_UZS")
+    button1 = types.KeyboardButton(text="RUB")
+    button2 = types.KeyboardButton(text="USD")
+    button3 = types.KeyboardButton(text="EVRO")
 
     kb.add(button1)
     kb.add(button2)
+    kb.add(button3)
     return kb
--- konverter_valut_bot.py
+++ konverter_valut_bot.py
@@ -2,7 +2,6 @@ import telebot
 import buttons
 
 bot = telebot.TeleBot(token = "7551169428:AAHtv4aXVn90FpysBZn78TjWUEwl1nnM51w")
-
 @bot.message_handler(commands=["start"])
 def start(message):
     user_id = message.from_user.id
@@ -14,30 +13,40 @@ def get_name(message):
     user_id = message.from_user.id
     name = message.text
     bot.send_message(user_id, "Выберите действие!", reply_markup=buttons.main_menu())
-    bot.register_next_step_handler(message, change, name)
-def change(message, name):
+    bot.register_next_step_handler(message, change)
+def change(message):
     user_id = message.from_user.id
-    if message.text == "UZS_USD":
+    if message.text == "RUB":
         bot.send_message(user_id, "Введите сумму: ")
-        bot.register_next_step_handler(message, change_uzs)
-    elif message.text == "USD_UZS":
+        bot.register_next_step_handler(message,change_ru)
+    elif message.text == "USD":
         bot.send_message(user_id, "Введите сумму: ")
         bot.register_next_step_handler(message, change_usd)
-def change_uzs(message):
+    elif message.text == "EVRO":
+        bot.send_message(user_id, "Введите сумму: ")
+        bot.register_next_step_handler(message, change_evro)
+
+def change_ru(message):
+    user_id = message.from_user.id
+    uzs = float(message.text)
+    result = uzs // 131
+    bot.send_message(user_id, f"{result} рублей")
+    bot.send_message(user_id, f"Выберите действие:", reply_markup=buttons.main_menu())
+    bot.register_next_step_handler(message, change)
+
+def change_usd(message):
     user_id = message.from_user.id
     uzs = float(message.text)
     result = uzs // 12600
-    bot.send_message(user_id, f"{uzs} сум = {result}$")
-def change_usd(message):
+    bot.send_message(user_id, f"{result} долларов")
+    bot.send_message(user_id, f"Выберите действие:", reply_markup=buttons.main_menu())
+    bot.register_next_step_handler(message, change)
+def change_evro(message):
     user_id = message.from_user.id
-    usd = float(message.text)
-    result = usd * 12600
-    bot.send_message(user_id, f"{usd}$ = {result}сум")
-
-
-
-
-
-
+    uzs = float(message.text)
+    result = uzs // 13807
+    bot.send_message(user_id, f"{result} евро")
+    bot.send_message(user_id, f"Выберите действие:", reply_markup=buttons.main_menu())
+    bot.register_next_step_handler(message, change)
 
 bot.infinity_polling()
